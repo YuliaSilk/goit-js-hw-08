a2215c79382995e20430f4c565792d71273cba1a
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-08-13 13:26:47 +0300

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,41 @@
-import Player from '@vimeo/player';
-import thorttle from 'lodash.throttle';
-const Vimeo = require('@vimeo/player');
-const throttle = require('lodash.throttle');
-
+import Player from '@vimeo/player'; 
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-// player.on('eventName', function(data) {
-//     // data is an object containing properties specific to that event
-// });
+const player = new Player(iframe);
 player.on('timeupdate', throttle(function(data) {
     const currentTime = data.seconds;
     localStorage.setItem('videoplayer-current-time', currentTime);
 }, 1000));
-
 window.onload = function () {
     const savedTime = localStorage.getItem('videoplayer-current-time');
     if (savedTime !== null) {
         player.setCurrentTime(savedTime);
     }
 };
+
+
+
+// import Player from '@vimeo/player';
+// import thorttle from 'lodash.throttle';
+// const Vimeo = require('@vimeo/player');
+// const throttle = require('lodash.throttle');
+
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+// // player.on('eventName', function(data) {
+// //     // data is an object containing properties specific to that event
+// // });
+// player.on('timeupdate', throttle(function(data) {
+//     const currentTime = data.seconds;
+//     localStorage.setItem('videoplayer-current-time', currentTime);
+// }, 1000));
+
+// window.onload = function () {
+//     const savedTime = localStorage.getItem('videoplayer-current-time');
+//     if (savedTime !== null) {
+//         player.setCurrentTime(savedTime);
+//     }
+// };
 // player.getVideoTitle().then(function(title) {
 //     console.log('title:', title);
 // },);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,7 @@
 import throttle from 'lodash.throttle';
-
 const emailInput = document.querySelector('input[name="email"]');
-const messageInput = document.querySelector('textarea[name="name"]');
-
+const messageInput = document.querySelector('textarea[name="message"]');
+const submitButton = document.querySelector('button[type="submit"]');
 function saveFormStateToLocalStorage() {
     const formData = {
         email: emailInput.value,
@@ -10,43 +9,35 @@ function saveFormStateToLocalStorage() {
     };
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 }
-
 function restoreFormStateFromLocalStorage() {
     const formDataString = localStorage.getItem("feedback-form-state");
     if (formDataString) {
         const formData = JSON.parse(formDataString);
-       emailInput.value = formData.email;
-       messageInput.value = formData.message;
+        emailInput.value = formData.email;
+        messageInput.value = formData.message;
     }
 }
 function clearLocalStorage() {
     localStorage.removeItem("feedback-form-state");
 }
-
 function handleInputEvent() {
     saveFormStateToLocalStorageThrottled();
 }
 const saveFormStateToLocalStorageThrottled = throttle(saveFormStateToLocalStorage, 500);
-
 function handleSubmitEvent(evt) {
     evt.preventDefault();
     clearLocalStorage();
-
     const formData = {
         email: emailInput.value,
         message: messageInput.value,
-      };
-      console.log(formData);
+    };
+    console.log(formData);
+    emailInput.value = '';
+    messageInput.value = '';
 }
- 
 document.addEventListener('DOMContentLoaded', () => {
     restoreFormStateFromLocalStorage();
-    const emailInput = document.querySelector('input[name="email"]');
-  const messageInput = document.querySelector('textarea[name="message"]');
-  const submitButton = document.querySelector('button[type="submit"]');
-
-  emailInput.addEventListener('input', handleInputEvent);
-  messageInput.addEventListener('input', handleInputEvent);
-  submitButton.addEventListener('click', handleSubmitEvent);
+    emailInput.addEventListener('input', handleInputEvent);
+    messageInput.addEventListener('input', handleInputEvent);
+    submitButton.addEventListener('click', handleSubmitEvent);
 });
-
